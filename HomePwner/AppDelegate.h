327549d86d5e6c76377e2824acb18f34eddf395c
Sander Peerna <sanderpeerna@gmail.com>
2015-06-19 13:34:52 -0700

    Initial Commit

---
+++ HomePwner/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HomePwner
+//
+//  Created by Sander Peerna on 6/19/15.
+//  Copyright (c) 2015 Sander Peerna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
