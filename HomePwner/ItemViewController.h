31754dae6d1155032c9cd93455d6ef6725898525
Sander Peerna <sanderpeerna@gmail.com>
2015-06-19 13:38:59 -0700

    Initial commit.

--- HomePwner/ItemViewController.h
+++ HomePwner/ItemViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  ItemViewController.h
 //  HomePwner
 //
 //  Created by Sander Peerna on 6/19/15.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface ItemViewController : UITableViewController
 
 @end
-
